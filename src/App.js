f0cef9cb83d9795a52ccf3ed61614d4388c46b28
Mike Gusev <mihail.gusiev@gmail.com>
2023-12-26 15:56:59 +0200

    timers demo functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
 import Timer from './components/Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <Timer maxTime={6} />
-    </div>
-  );
-}
+import './App.css';
+
+const App = () => {
+    const [timers, setTimers] = useState([]);
+    const [maxDuration, setMaxDuration] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
+    // Load state from localStorage
+    useEffect(() => {
+        const savedTimers = JSON.parse(localStorage.getItem('timers'));
+        if (savedTimers) {
+            setTimers(savedTimers);
+        }
+    }, []);
+
+    // Save state to localStorage
+    useEffect(() => {
+        localStorage.setItem('timers', JSON.stringify(timers));
+    }, [timers]);
+
+    const addTimer = () => {
+        const duration = parseInt(prompt("Enter duration in seconds:", "60"), 10);
+        if (!isNaN(duration)) {
+            setTimers([...timers, { duration, id: Date.now(), startDelay: 0 }]);
+            setMaxDuration(Math.max(maxDuration, duration));
+        }
+    };
+
+    const startTimers = () => {
+        const updatedTimers = timers.map(timer => ({
+            ...timer,
+            startDelay: maxDuration - timer.duration
+        }));
+        setTimers(updatedTimers);
+        setIsRunning(true);
+    };
+
+    const pauseResumeTimers = () => {
+        setIsRunning(!isRunning);
+    };
+
+    const resetTimers = () => {
+        setTimers([]);
+        setMaxDuration(0);
+        setIsRunning(false);
+    };
+
+    return (
+        <div className="app">
+            <div className="timer-controls">
+                <button onClick={addTimer}>Add New</button>
+                <button onClick={startTimers}>Start</button>
+                <button onClick={pauseResumeTimers}>{isRunning ? 'Pause' : 'Resume'}</button>
+                <button onClick={resetTimers}>Reset</button>
+            </div>
+            <div className="timer-container">
+                {timers.map(timer => (
+                    <Timer
+                        key={timer.id}
+                        duration={timer.duration}
+                        startDelay={timer.startDelay}
+                        isRunning={isRunning}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,43 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import './Timer.css';
 
-const Timer = ({ maxTime }) => {
-    const [time, setTime] = useState(0);
-    const radius = 45; // Radius of the SVG circle
-    const circumference = 2 * Math.PI * radius; // Circumference of the circle
+const Timer = ({ duration, startDelay, isRunning }) => {
+    const [timeElapsed, setTimeElapsed] = useState(0);
+    const [timerActive, setTimerActive] = useState(false);
+    let interval = null;
+    const radius = 45;
+    const circumference = 2 * Math.PI * radius;
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(prevTime => (prevTime < maxTime ? prevTime + 1 : maxTime));
-        }, 1000);
+        if (isRunning && !timerActive) {
+            setTimeout(() => {
+                setTimerActive(true);
+                interval = setInterval(() => {
+                    setTimeElapsed((prevTime) => prevTime + 1);
+                }, 1000);
+            }, startDelay * 1000);
+        }
 
-        return () => clearInterval(interval);
-    }, [maxTime]);
+        // Clearing the interval when component unmounts or timer is stopped
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+            setTimerActive(false);
+        };
+    }, [isRunning, startDelay]);
 
-    // Calculate the stroke dash offset based on the timer
-    const strokeDashoffset = circumference - (time / maxTime) * circumference;
+    useEffect(() => {
+        if (timeElapsed >= duration) {
+            if (interval) {
+                clearInterval(interval);
+            }
+            setTimerActive(false);
+            setTimeElapsed(duration);
+        }
+    }, [timeElapsed, duration]);
+
+    const formatTime = (seconds) => {
+        const mins = Math.floor(seconds / 60).toString().padStart(2, '0');
+        const secs = (seconds % 60).toString().padStart(2, '0');
+        return `${mins}:${secs}`;
+    };
+
+    const strokeDashoffset = () => {
+        // Calculate the offset based on the time elapsed
+        return circumference - (timeElapsed / duration) * circumference;
+    };
 
     return (
         <div className="timer">
             <svg className="timer-svg" width="100" height="100">
-                <circle
-                    className="timer-circle-bg"
-                    cx="50" cy="50" r={radius}
-                    fill="black" />
+                <circle className="timer-circle-bg" cx="50" cy="50" r="45" />
                 <circle
                     className="timer-circle"
-                    cx="50" cy="50" r={radius}
-                    fill="transparent"
-                    stroke="cyan"
-                    strokeWidth="3"
+                    cx="50" cy="50" r="45"
                     strokeDasharray={circumference}
-                    strokeDashoffset={strokeDashoffset}
-                    transform="rotate(-90 50 50)"
+                    strokeDashoffset={strokeDashoffset()}
                 />
             </svg>
             <span className="timer-text">
-        {new Date(time * 1000).toISOString().substr(14, 5)}
-      </span>
+                {formatTime(timeElapsed)}
+            </span>
         </div>
     );
 };
